9f430ddad1e9516921fcef1e6cdfe75ff5fdd64e
Carly Orr <carly_wong_orr@bcit.ca>
2023-01-16 12:44:50 -0800

    Create files for JS Challenge #1

---
+++ myscript.js
@@ -0,0 +1,8 @@
+// Function to output a console message and create an alert popup.
+function sayHello() {
+    console.log("hello world!");
+    alert("hello world!");
+}
+
+// Call the function sayHello()
+sayHello();
