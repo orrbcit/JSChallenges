de5e587181a36de2b55b60ad12bde6e229990c8d
Carly Orr <carly_wong_orr@bcit.ca>
2023-01-20 15:34:27 -0800

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,6 @@
+// Function to output a console message and create an alert popup.
+function myHelloFunction() {
+    console.log("hello");
+    alert("hello");
+}
+//no need to call this function, because it will be called (invoked when a button is clicked)
