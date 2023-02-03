37b04b6ed8c59e821048b5bc726c4b5f51f7479e
Hoda Rashedi <hrashedi1@bcit.ca>
2023-02-03 09:40:44 -0800

    JS4 updated

--- myscript.js
+++ myscript.js
@@ -1,7 +1,14 @@
 // Function to output a console message and create an alert popup.
 function myFunction() {
-    let nameInput = prompt("Enter your name:");  //This code creates a variable named "nameInput" and assigns the value entered by the user in the browser's prompt dialog box to it.
-    console.log("Your name is ${nameInput}");
-    document.getElementById("name").innerHTML = nameInput; //select an element on the HTML page with the id of "name". Then it sets the innerHTML property of that element to the value stored in the variable "nameInput".
+    let nameInput = document.getElementById( "answer" ).value;
+    console.log( nameInput );
+    let text = "Your input is " + nameInput + "\nOK or cancel";;
+    if ( confirm( text ) == true ) {
+        text = "You pressed OK!";
+        document.getElementById( "name" ).innerHTML = nameInput;
+    } else {
+        text = "You canceled!";
+    }
+    document.getElementById( "messageUpdate" ).innerHTML = text;
 }
 //no need to call this function, because it will be called (invoked when a button is clicked)
