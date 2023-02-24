b2b7ee932d44343e75598665e9d4f454cc6b4264
Carly Orr <carly_wong_orr@bcit.ca>
2023-02-23 20:58:07 -0800

    ad url with parameter code

--- details.js
+++ details.js
@@ -1,8 +1,19 @@
-function showDetails(){
+function showDetails_LocalStorage(){
     var item = localStorage.getItem("item");
     console.log(item);
     var message = "Let me tell you more about " + item;
     document.getElementById("details-go-here").innerHTML = message;
 }
 
-showDetails();
+//showDetails_LocalStorage();
+
+function showDetails_URL(){
+    const myURL = new URLSearchParams(document.location.search);
+    const item = myURL.get('item');
+    console.log(item);
+
+    var message = "Let me tell you more about " + item;
+    document.getElementById("details-go-here").innerHTML = message;
+}
+
+showDetails_URL();
--- menu.js
+++ menu.js
@@ -1,14 +1,19 @@
 function addMenuListener(item){
     console.log(item);
     document.getElementById(item).addEventListener("click", ()=>{
-        passValue(item);
+        passValueURL(item);
     })
 }
 
-function passValue(item){
+function passValueLocalStorage(item){
     localStorage.setItem("item", item);
     console.log("clicked ..." + item);
     window.location.href = "./details.html";
+}
+
+function passValueURL(item){
+    console.log("clicked ..." + item);
+    window.location.href = "./details.html?item=" + item;
 }
 
 addMenuListener("coffee");
